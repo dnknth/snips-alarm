b11117cdfb4d0572fd09d48158a41a1b8aa51d3d
Daniel Knauth <dk+git@krachbumm.de>
2019-10-24 10:49:00 +0200

    Fix setup.sh

--- setup.sh
+++ setup.sh
@@ -2,15 +2,13 @@
 
 set -e
 
-VENV="venv"
-
 if which python3 > /dev/null ; then
 
     # Create a virtual environment if it doesn't exist.
-    [ -d $VENV ] || $PYTHON -m venv $VENV
+    [ -d venv ] || python3 -m venv venv
 
     # Activate the virtual environment and install requirements.
-    . $VENV/bin/activate
+    . venv/bin/activate
     pip3 install -r requirements.txt
 else
     >&2 echo "Cannot find Python3. Please install it."
